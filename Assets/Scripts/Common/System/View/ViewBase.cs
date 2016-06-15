f706d14987d18956a8efefc627c1661c4aaa7bb4
PCD-00154 <PCD-00154@PCD-00154.local>
2016-06-15 09:41:15 +0900

    サーバー周り一部機能追加

--- Assets/Scripts/Common/System/View/ViewBase.cs
+++ Assets/Scripts/Common/System/View/ViewBase.cs
@@ -8,16 +8,34 @@ using System.Collections.Generic;
 /// </summary>
 public abstract class ViewBase : MonoBehaviour, IDisposable
 {
-	private Dictionary<Type, Dictionary<string, object>>	Scripts
+
+	/// <summary>
+	/// プロパティ：ボタン有効設定
+	/// </summary>
+	public bool IsEnableButton
 	{
-		get{
-			if( this.m_scripts == null ){
-				this.m_scripts	= new Dictionary<Type, Dictionary<string, object>>();
+		set {
+			foreach(var b in this.GetComponentsInChildren<UIButton>(true)){
+				b.enabled = value;
 			}
-			return m_scripts;
+			foreach(var b in this.GetComponentsInChildren<UIImageButton>(true)){
+				b.enabled = value;
+			}
 		}
 	}
-	private Dictionary<Type, Dictionary<string, object>> m_scripts;
+
+	/// <summary>
+	/// プロパティ：表示有効設定
+	/// </summary>
+	public bool IsVisible
+	{
+		set {
+			foreach(var p in this.GetComponentsInChildren<UIPanel>(true)){
+				p.enabled = value;
+			}
+			this.IsEnableButton = value;
+		}
+	}
 
 	/// <summary>
 	/// アンマネージド系のリソースを使うことを考慮
@@ -38,30 +56,9 @@ public abstract class ViewBase : MonoBehaviour, IDisposable
 	}
 
 	/// <summary>
-	/// 子階層にあるすべてのスクリプトから該当名のものを取得する.
+	/// 指定タイプのコンポーネントを子階層から全て取得する
 	/// </summary>
-	public T GetScript<T>(string key) where T : Component
-	{
-		if(!this.Scripts.ContainsKey(typeof(T)) ){
-			this.UpdateScriptList<T>();
-		}
-		var	tbl	= this.Scripts[typeof(T)];
-		
-#if DEBUG
-		if(!tbl.ContainsKey(key) ){
-			Debug.LogError("GetComponent KeyError:"+key);
-		}
-#endif
-		return tbl[key] as T;
-	}
-	private void UpdateScriptList<T>() where T : Component
-	{
-		if( this.Scripts.ContainsKey(typeof(T)) ){
-			this.Scripts[typeof(T)].Clear();
-		}
-		this.Scripts[typeof(T)]	= this.GetScriptList(typeof(T));
-	}
-	private Dictionary<string, object> GetScriptList(Type type)
+	public Dictionary<string, object> GetScriptList(Type type)
 	{
 		var	tbl	= new Dictionary<string, object>();
 		
@@ -74,5 +71,35 @@ public abstract class ViewBase : MonoBehaviour, IDisposable
 		}
 		return tbl;
 	}
+
+	/// <summary>
+	/// 子階層にあるすべてのスクリプトから該当名のものを取得する.
+	/// </summary>
+	public T GetScript<T>(string key) where T : Component
+	{
+		if(!this.Scripts.ContainsKey(typeof(T)) ){
+			this.UpdateScriptList<T>();
+		}
+		var	tbl	= this.Scripts[typeof(T)];
+		return tbl[key] as T;
+	}
+	private void UpdateScriptList<T>() where T : Component
+	{
+		if( this.Scripts.ContainsKey(typeof(T)) ){
+			this.Scripts[typeof(T)].Clear();
+		}
+		this.Scripts[typeof(T)]	= this.GetScriptList(typeof(T));
+	}
+
+	private Dictionary<Type, Dictionary<string, object>>	Scripts
+	{
+		get{
+			if( this.m_scripts == null ){
+				this.m_scripts	= new Dictionary<Type, Dictionary<string, object>>();
+			}
+			return m_scripts;
+		}
+	}
+	private Dictionary<Type, Dictionary<string, object>> m_scripts;
 
 }
