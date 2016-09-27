35788bc47d150e7e3a45218ec5d2185722074ab3
ryo <moge.ima765@gmail.com>
2016-09-27 17:24:41 +0900

    不要なデータ削除。Define系のクラスを追加。

---
+++ Assets/SystemDataDefine/DefineLayerNo.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// AutoOutputFromEditor : レイヤー番号を定数で管理するクラス
+/// </summary>
+public static class DefineLayerNo
+{
+	  public const int DEFAULT        = 0;
+	  public const int TRANSPARENT_FX = 1;
+	  public const int IGNORE_RAYCAST = 2;
+	  public const int WATER          = 4;
+	  public const int UI             = 5;
+}
---
+++ Assets/SystemDataDefine/DefineTagName.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// AutoOutputFromEditor : タグ名を定数で管理するクラス
+/// </summary>
+public static class DefineTagName
+{
+	  public const string UNTAGGED        = "Untagged";
+	  public const string RESPAWN         = "Respawn";
+	  public const string FINISH          = "Finish";
+	  public const string EDITOR_ONLY     = "EditorOnly";
+	  public const string MAIN_CAMERA     = "MainCamera";
+	  public const string PLAYER          = "Player";
+	  public const string GAME_CONTROLLER = "GameController";
+}
