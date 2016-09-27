03cbe7c769ab924fd759749a8cd1993c0a1d3d18
ryo <moge.ima765@gmail.com>
2016-09-27 17:24:41 +0900

    不要なデータ削除。Define系のクラスを追加。

---
+++ Assets/SystemDataDefine/DefineLayerMaskNo.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// AutoOutputFromEditor : レイヤーマスク番号を定数で管理するクラス
+/// </summary>
+public static class DefineLayerMaskNo
+{
+	  public const int DEFAULT        = 1;
+	  public const int TRANSPARENT_FX = 2;
+	  public const int IGNORE_RAYCAST = 4;
+	  public const int WATER          = 16;
+	  public const int UI             = 32;
+}
---
+++ Assets/SystemDataDefine/DefinePlayerSettings.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// AutoOutputFromEditor : PlayerSettingsの設定を定数で管理するクラス
+/// </summary>
+public static class DefinePlayerSettings
+{
+	  public const string PRODUCT_NAME   = "MyUtilityFramework_UnityProject";
+	  public const string BUNDLE_VERSION = "1.0";
+}
